8cf069639291d76778fc71fe0c1bc870e1618e35
Corey Paskewitz <cpaskewitz@Coreys-MacBook-Pro.local>
2024-06-18 17:26:10 -0400

    readd app.scss

--- TaskMaster-Frontend/src/App.tsx
+++ TaskMaster-Frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from './pages/Auth/Auth';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import { Home } from './pages/Home/Home';
+import './App.scss';
 
 function App() {
   
